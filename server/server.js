cd3fed7aa917792321f2eaad97e6d88953dc96a6
AbdElRahman-7 <abdelrahmanhamdy1999@gmail.com>
2026-04-28 22:40:54 +0300

    fix token

--- server/server.js
+++ server/server.js
@@ -14,8 +14,18 @@ if (!process.env.JWT_SECRET) {
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
+  .then(async () => {
     console.log("Connected to MongoDB");
+
+    // TEMPORARY: Automatically upgrade all users to admin so you don't have to run scripts!
+    try {
+      const User = require('./models/user.model');
+      await User.updateMany({}, { role: "admin" });
+      console.log("SUCCESS: All users have been upgraded to Admin in the database!");
+    } catch (e) {
+      console.error("Failed to upgrade users:", e);
+    }
+
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
